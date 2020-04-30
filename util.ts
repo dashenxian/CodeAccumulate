c698f92831e966034e2d95a6d3f185bf7959350c
dashenxian <125880321@qq.com>
2020-04-30 11:02:14 +0800

    util添加数组去重、delay
    util添加数组去重、delay

--- util.ts
+++ util.ts
@@ -154,6 +154,30 @@ class Util {
       ? str.substr(0, str.length - remove.length)
       : str;
   }
+  joint(arr: string[], spilt: string = ",") {
+    let str = "";
+    arr.map((i) => (str += i + spilt));
+    str = this.removeEnd(str, spilt);
+    return str;
+  }
+  /**数组去重 */
+  unique(arr) {
+    let unique = {};
+    arr.forEach(function(item) {
+      unique[JSON.stringify(item)] = item; //键名不会重复
+    });
+    arr = Object.keys(unique).map(function(u) {
+      //Object.keys()返回对象的所有键值组成的数组，map方法是一个遍历方法，返回遍历结果组成的数组.将unique对象的键名还原成对象数组
+      return JSON.parse(u);
+    });
+    return arr;
+  }
+  /**类似与c#的Task.Delay() */
+  async delay(duration: number) {
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, duration);
+    });
+  }
 }
 const util = new Util();
 export default util;
