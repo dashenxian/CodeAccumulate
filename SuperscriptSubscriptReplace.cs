a76beda831efea3ae5db1216c9e28868810cd451
dashenxian <125880321@qq.com>
2020-05-29 13:43:27 +0800

    Update SuperscriptSubscriptReplace.cs

--- SuperscriptSubscriptReplace.cs
+++ SuperscriptSubscriptReplace.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Text.RegularExpressions;
     public static class SuperscriptSubscriptReplace
     {
+        
         /// <summary>
         /// 替换\u**\n为unicode上标
         /// </summary>
@@ -26,10 +27,38 @@ using System.Text.RegularExpressions;
                 { '-', '⁻' },
                 { '=', '⁼' },
                 { '(', '⁽' },
-                { ')', '⁾' }
+                { ')', '⁾' },
+                {'a','ᵃ'},
+                {'b','ᵇ'},
+                {'c','ᶜ'},
+                {'d','ᵈ'},
+                {'e','ᵉ'},
+                {'f','ᶠ'},
+                {'g','ᵍ'},
+                {'h','ʰ'},
+                {'i','ⁱ'},
+                {'j','ʲ'},
+                {'k','ᵏ'},
+                {'l','ˡ'},
+                {'m','ᵐ'},
+                {'n','ⁿ'},
+                {'o','ᵒ'},
+                {'p','ᵖ'},
+                {'q','ʳ'},
+                {'r','ʳ'},
+                {'s','ˢ'},
+                {'t','ᵗ'},
+                {'u','ᵘ'},
+                {'v','ᵛ'},
+                {'w','ʷ'},
+                {'x','ˣ'},
+                {'y','ʸ'},
+                {'z','ᶻ'},
             };
-            var reg = new Regex(@"\\[uU](.+?)\\n");
-            return Replace(replaceDic, str, reg);
+            //var reg = new Regex(@"\\[uU](.+?)\\n");
+            //return Replace(replaceDic, str, reg);
+
+            return ReplaceUp(replaceDic, str);
         }
         /// <summary>
         /// 替换\d**\n为unicode下标
@@ -55,9 +84,17 @@ using System.Text.RegularExpressions;
                 {'(', '₍' },
                 {')', '₎' },
             };
-            var reg = new Regex(@"\\[dD](.+?)\\n");
-            return Replace(replaceDic, str, reg);
+            //var reg = new Regex(@"\\[dD](.+?)\\n");
+            //return Replace(replaceDic, str, reg);
+            return ReplaceDown(replaceDic, str);
         }
+        /// <summary>
+        /// 使用正则替换，无法匹配到没有\n结尾的数据
+        /// </summary>
+        /// <param name="replaceDic"></param>
+        /// <param name="str"></param>
+        /// <param name="reg"></param>
+        /// <returns></returns>
         private static string Replace(Dictionary<char, char> replaceDic, string str, Regex reg)
         {
             var newStr = str;
@@ -67,11 +104,68 @@ using System.Text.RegularExpressions;
                 var replaceStr = "";
                 foreach (var item in match.Groups[1].Value)
                 {
-                    replaceStr += replaceDic[item];
+                    replaceStr += replaceDic.ContainsKey(item) ? replaceDic[item] : item;
                 }
                 newStr = newStr.Replace(match.Groups[0].Value, replaceStr);
             }
 
             return newStr;
+        }
+        /// <summary>
+        /// 循环替换
+        /// </summary>
+        /// <param name="replaceDic"></param>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        private static string ReplaceUp(Dictionary<char, char> replaceDic, string str)
+        {
+            return Replace(replaceDic, str, new List<char> { 'u', 'U' });
+        }
+        /// <summary>
+        /// 循环替换
+        /// </summary>
+        /// <param name="replaceDic"></param>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        private static string ReplaceDown(Dictionary<char, char> replaceDic, string str)
+        {
+            return Replace(replaceDic, str, new List<char> { 'd', 'D' });
+        }
+
+        /// <summary>
+        /// 循环替换
+        /// </summary>
+        /// <param name="replaceDic"></param>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        private static string Replace(Dictionary<char, char> replaceDic, string str, IEnumerable<char> specialCharacter)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+            var strNew = "";
+            var isReplace = false;
+            for (int i = 0; i < str.Length; i++)
+            {
+                //检测到"\u","\U","\d","\D",表示替换开始
+                if (str[i] == '\\' && i <= str.Length - 3
+                    && (specialCharacter.Contains(str[i + 1])))
+                {
+                    isReplace = true;
+                    i = i + 2;
+                }
+                //检测到"\n"并且替换开始,表示替换结束
+                if (isReplace && str[i] == '\\' && i < str.Length - 1 && str[i + 1] == 'n')
+                {
+                    isReplace = false;
+                    i = i + 2;
+                }
+                if (i < str.Length)
+                {
+                    strNew += isReplace && replaceDic.ContainsKey(str[i]) ? replaceDic[str[i]] : str[i];
+                }
+            }
+            return strNew;
         }
     }
